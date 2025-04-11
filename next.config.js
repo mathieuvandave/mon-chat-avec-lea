306f1f286635be37567e1c402ac6a876b41daa85
mathieuvandave <mattgarcia@hotmail.fr>
2025-04-11 13:42:10 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  exportPathMap: async function () {
-    const paths = {
-      '/api/send-welcome-message': { page: '/api/send-welcome-message' },
-    };
-    return paths;
-  },
-};
